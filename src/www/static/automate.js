9716263704faa0de4d867f7668151728b0bda539
Robert <rwallhead@gmail.com>
2014-08-23 15:34:52 +1000

    Partial Revert "Starting on some really ugly sensor display, needs to be a lot better :("
    This reverts commit 96d26ed7641ff630100de0f3b11f331ae9f93c55.
    
    Revert done to remove JS changes, but backend changes will be kept.

--- src/www/static/automate.js
+++ src/www/static/automate.js
@@ -2,13 +2,6 @@
 
     var scene, camera, renderer, controls, building;
 
-    var sensors = {};
-
-    var mat_sensor = new THREE.MeshBasicMaterial({
-        color: 0xffffff,
-        opacity: 0.75,
-    });
-
     var init = function (modelpath, zdepth) {
         scene = new THREE.Scene();
 
@@ -46,8 +39,6 @@
         renderer = new THREE.CanvasRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.domElement);
-
-        renderTelemetry();
     };
 
     var animate = function () {
@@ -59,66 +50,9 @@
         renderer.render(scene, camera);
     };
 
-    var renderTelemetry = function() {
-        var ajaxTicker = function() {
-            $.get('/current', function(json) {
-                $.each(json.current, function(id, value) {
-//                    console.log(sensors[id].mesh.material.map);
-//                    console.log(id, value);
-                });
-                render();
-            });
-        }
-        setInterval(function() {
-            ajaxTicker();
-        }, 1000);
-    }
-
-
-    var toScreenXY = function ( position, camera, jqdiv ) {
-
-        var pos = position.clone();
-        projScreenMat = new THREE.Matrix4();
-        projScreenMat.multiply( camera.projectionMatrix, camera.matrixWorldInverse );
-        projScreenMat.multiplyVector3( pos );
-
-        return { x: ( pos.x + 1 ) * jqdiv.width() / 2 + jqdiv.offset().left,
-             y: ( - pos.y + 1) * jqdiv.height() / 2 + jqdiv.offset().top };
-
-    }
-
-
-    var registorSensor = function(sensor) {
-
-        var sensorMesh = new THREE.Mesh(
-            new THREE.SphereGeometry(0.1, 8, 8),
-            mat_sensor
-        );
-        sensorMesh.position.x = sensor.config.x;
-        sensorMesh.position.y = sensor.config.y;
-        sensorMesh.position.z = sensor.config.z;
-        scene.add(sensorMesh);
-        sensor.mesh = sensorMesh;
-        sensors[sensor.config.id] = {
-            title: sensor.title,
-            mesh: sensorMesh,
-        };
-    }
-
     automate.loadBuilding = function (modelpath, zdepth) {
         init(modelpath, zdepth);
         animate();
-    };
-
-    automate.addSensors = function() {
-        $.get('/sensors', function(json) {
-            $.each(json.sensors, function(title, config) {
-                registorSensor({
-                    title: title,
-                    config: config,
-                });
-            });
-        });
     };
 
 }( window.automate = window.automate || {}, automate));
